6baeb9d04566a5eba5297d92fa1830074082d9b2
David Deutsch <skore@valanx.org>
2014-07-08 14:49:34 +0200

    temp menu update

--- wordpress/wp-content/themes/wordpress-bootstrap-master/header.php
+++ wordpress/wp-content/themes/wordpress-bootstrap-master/header.php
@@ -51,9 +51,9 @@
 					</div>
 				</form>-->
 				<ul class="nav navbar-nav">
-					<li><a href="#">Forside</a></li>
-					<li><a href="#">Om oss</a></li>
-					<li><a href="#">Kvalitet/HMS/Miljø</a></li>
+					<li><a href="/">Forside</a></li>
+					<li><a href="/?page_id=8">Om oss</a></li>
+					<li><a href="/?page_id=14">Kvalitet/HMS/Miljø</a></li>
 					<li><a href="#">Tjenester</a></li>
 					<li><a href="#">Aktuelt</a></li>
 					<li><a href="#">Befaring</a></li>

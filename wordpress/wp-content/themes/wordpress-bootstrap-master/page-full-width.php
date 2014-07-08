dc201135f19f39c765bfe382cf603bbc3d22ae2c
David Deutsch <skore@valanx.org>
2014-07-08 14:41:40 +0200

    adding header image and box to fullwidth pages

--- wordpress/wp-content/themes/wordpress-bootstrap-master/page-full-width.php
+++ wordpress/wp-content/themes/wordpress-bootstrap-master/page-full-width.php
@@ -6,37 +6,42 @@ Template Name: Full Width Page
 
 <?php get_header(); ?>
 
-			<div id="content" class="clearfix row">
+			<div class="col-md-12 img-wrap">
+				<img class="placeholder" src="<?php echo get_template_directory_uri(); ?>/library/img/vaktmesterservice.png" alt=""/>
+			</div>
+			<div class="col-md-8 col-md-offset-2">
+				<div class="panel panel-default panel-standalone">
+					<div class="panel-body">
 
-				<div id="main" class="col col-lg-12 clearfix" role="main">
+						<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
-					<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+						<article id="post-<?php the_ID(); ?>" <?php post_class('clearfix'); ?> role="article">
 
-					<article id="post-<?php the_ID(); ?>" <?php post_class('clearfix'); ?> role="article">
+							<section class="post_content">
+								<?php the_content(); ?>
 
-						<section class="post_content">
-							<?php the_content(); ?>
+							</section> <!-- end article section -->
 
-						</section> <!-- end article section -->
+						</article> <!-- end article -->
 
-					</article> <!-- end article -->
+						<?php endwhile; ?>
 
-					<?php endwhile; ?>
+						<?php else : ?>
 
-					<?php else : ?>
+						<article id="post-not-found">
+							<header>
+								<h1><?php _e("Not Found", "wpbootstrap"); ?></h1>
+							</header>
+							<section class="post_content">
+								<p><?php _e("Sorry, but the requested resource was not found on this site.", "wpbootstrap"); ?></p>
+							</section>
+							<footer>
+							</footer>
+						</article>
 
-					<article id="post-not-found">
-					    <header>
-					    	<h1><?php _e("Not Found", "wpbootstrap"); ?></h1>
-					    </header>
-					    <section class="post_content">
-					    	<p><?php _e("Sorry, but the requested resource was not found on this site.", "wpbootstrap"); ?></p>
-					    </section>
-					    <footer>
-					    </footer>
-					</article>
+						<?php endif; ?>
 
-					<?php endif; ?>
+					</div>
 
 				</div> <!-- end #main -->
 

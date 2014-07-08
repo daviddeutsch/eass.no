febd21bc28293dc1741db0dfe12e8a93cfa8bd07
David Deutsch <skore@valanx.org>
2014-07-08 14:24:02 +0200

    working on homepage

--- wordpress/wp-content/themes/wordpress-bootstrap-master/page-homepage.php
+++ wordpress/wp-content/themes/wordpress-bootstrap-master/page-homepage.php
@@ -5,78 +5,53 @@ Template Name: Homepage
 ?>
 
 <?php get_header(); ?>
-			
+
 			<div id="content" class="clearfix row">
-			
+
 				<div id="main" class="col-sm-12 clearfix" role="main">
-
-					<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-					
-					<article id="post-<?php the_ID(); ?>" <?php post_class('clearfix'); ?> role="article">
-					
-						<header>
-
-							<?php 
-								$post_thumbnail_id = get_post_thumbnail_id();
-								$featured_src = wp_get_attachment_image_src( $post_thumbnail_id, 'wpbs-featured-home' );
-							?>
-
-							<div class="jumbotron" style="background-image: url('<?php echo $featured_src[0]; ?>'); background-repeat: no-repeat; background-position: 0 0;">
-				
-								<div class="page-header">
-									<h1><?php bloginfo('title'); ?><small><?php echo get_post_meta($post->ID, 'custom_tagline' , true);?></small></h1>
-								</div>				
-								
+					<div ng-controller="HomeImgCtrl">
+						<div class="col-md-12 img-wrap">
+							<img ng-repeat="slide in slides" class="placeholder am-fade" ng-src="{{ '<?php echo get_template_directory_uri(); ?>/library/img/'+slide.name+'.png' }}" alt="" ng-hide="!isCurrent($index)"/>
+						</div>
+						<div class="col-md-4" ng-mouseover="change(0)" ng-mouseleave="reset()">
+							<div class="panel panel-default" ng-class="{raised:isCurrent(0)}">
+								<div class="panel-body">
+									<h3>Renhold</h3>
+									<p>Vår renholdsavdeling er bygget opp av de mest erfarne fagfolk i vårt distrikt. Vår ren- holdsavdeling er bygget opp av de mest erfarne fagfolk i vårt distrikt. Vår ren- holdsavdeling er bygget opp av de mest erfarne fagfolk i vårt distrikt.</p>
+									<hr/>
+									<h4 class="pull-right">
+										<a href="#">Les mer <i class="fa fa-chevron-right"></i></a>
+									</h4>
+								</div>
 							</div>
-						
-						</header>
-						
-						<section class="row post_content">
-						
-							<div class="col-sm-8">
-						
-								<?php the_content(); ?>
-								
+						</div>
+						<div class="col-md-4" ng-mouseover="change(1)" ng-mouseleave="reset()">
+							<div class="panel panel-default" ng-class="{raised:isCurrent(1)}">
+								<div class="panel-body">
+									<h3>Kantine</h3>
+									<p>Vår renholdsavdeling er bygget opp av de mest erfarne fagfolk i vårt distrikt. Vår ren- holdsavdeling er bygget opp av de mest erfarne fagfolk i vårt distrikt. Vår ren- holdsavdeling er bygget opp av de mest erfarne fagfolk i vårt distrikt.</p>
+									<hr/>
+									<h4 class="pull-right">
+										<a href="#">Les mer <i class="fa fa-chevron-right"></i></a>
+									</h4>
+								</div>
 							</div>
-							
-							<?php get_sidebar('sidebar2'); // sidebar 2 ?>
-													
-						</section> <!-- end article header -->
-						
-						<footer>
-			
-							<p class="clearfix"><?php the_tags('<span class="tags">' . __("Tags","wpbootstrap") . ': ', ', ', '</span>'); ?></p>
-							
-						</footer> <!-- end article footer -->
-					
-					</article> <!-- end article -->
-					
-					<?php 
-						// No comments on homepage
-						//comments_template();
-					?>
-					
-					<?php endwhile; ?>	
-					
-					<?php else : ?>
-					
-					<article id="post-not-found">
-					    <header>
-					    	<h1><?php _e("Not Found", "wpbootstrap"); ?></h1>
-					    </header>
-					    <section class="post_content">
-					    	<p><?php _e("Sorry, but the requested resource was not found on this site.", "wpbootstrap"); ?></p>
-					    </section>
-					    <footer>
-					    </footer>
-					</article>
-					
-					<?php endif; ?>
-			
-				</div> <!-- end #main -->
-    
-				<?php //get_sidebar(); // sidebar 1 ?>
-    
+						</div>
+						<div class="col-md-4" ng-mouseover="change(2)" ng-mouseleave="reset()">
+							<div class="panel panel-default" ng-class="{raised:isCurrent(2)}">
+								<div class="panel-body">
+									<h3>Vaktmesterservice</h3>
+									<p>Vår renholdsavdeling er bygget opp av de mest erfarne fagfolk i vårt distrikt. Vår ren- holdsavdeling er bygget opp av de mest erfarne fagfolk i vårt distrikt. Vår ren- holdsavdeling er bygget opp av de mest erfarne fagfolk i vårt distrikt.</p>
+									<hr/>
+									<h4 class="pull-right">
+										<a href="#">Les mer <i class="fa fa-chevron-right"></i></a>
+									</h4>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+
 			</div> <!-- end #content -->
 
 <?php get_footer(); ?>

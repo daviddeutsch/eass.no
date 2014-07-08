99d456a070abe382f70c457d7087d6b70609c7fb
David Deutsch <skore@valanx.org>
2014-07-08 14:37:45 +0200

    adding favicon

--- wordpress/wp-content/themes/wordpress-bootstrap-master/header.php
+++ wordpress/wp-content/themes/wordpress-bootstrap-master/header.php
@@ -10,6 +10,9 @@
 		<meta charset="utf-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 		<title><?php wp_title( '|', true, 'right' ); ?></title>
+
+		<link href="favicon.ico" rel="shortcut icon" type="image/vnd.microsoft.icon">
+
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
   		<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
 

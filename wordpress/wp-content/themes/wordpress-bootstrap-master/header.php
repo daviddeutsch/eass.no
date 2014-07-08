bf58000a728ef83d8c4942ca4224ca4d34d1252e
David Deutsch <skore@valanx.org>
2014-07-08 13:51:06 +0200

    adding scripts

--- wordpress/wp-content/themes/wordpress-bootstrap-master/header.php
+++ wordpress/wp-content/themes/wordpress-bootstrap-master/header.php
@@ -1,15 +1,15 @@
-<!doctype html>  
+<!doctype html>
 
 <!--[if IEMobile 7 ]> <html <?php language_attributes(); ?>class="no-js iem7"> <![endif]-->
 <!--[if lt IE 7 ]> <html <?php language_attributes(); ?> class="no-js ie6"> <![endif]-->
 <!--[if IE 7 ]>    <html <?php language_attributes(); ?> class="no-js ie7"> <![endif]-->
 <!--[if IE 8 ]>    <html <?php language_attributes(); ?> class="no-js ie8"> <![endif]-->
 <!--[if (gte IE 9)|(gt IEMobile 7)|!(IEMobile)|!(IE)]><!--><html <?php language_attributes(); ?> class="no-js"><!--<![endif]-->
-	
+
 	<head>
 		<meta charset="utf-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-		<title><?php wp_title( '|', true, 'right' ); ?></title>	
+		<title><?php wp_title( '|', true, 'right' ); ?></title>
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
   		<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
 
@@ -19,27 +19,31 @@
 		<!-- IE8 fallback moved below head to work properly. Added respond as well. Tested to work. -->
 			<!-- media-queries.js (fallback) -->
 		<!--[if lt IE 9]>
-			<script src="http://css3-mediaqueries-js.googlecode.com/svn/trunk/css3-mediaqueries.js"></script>			
+			<script src="http://css3-mediaqueries-js.googlecode.com/svn/trunk/css3-mediaqueries.js"></script>
 		<![endif]-->
 
 		<!-- html5.js -->
 		<!--[if lt IE 9]>
 			<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
-		<![endif]-->	
-		
+		<![endif]-->
+
 			<!-- respond.js -->
 		<!--[if lt IE 9]>
 		          <script type='text/javascript' src="http://cdnjs.cloudflare.com/ajax/libs/respond.js/1.4.2/respond.js"></script>
-		<![endif]-->	
+		<![endif]-->
+
+		<script src="themes/wordpress-bootstrap-master/library/js/angular.min.js"></script>
+		<script src="themes/wordpress-bootstrap-master/library/js/angular-animate.min.js"></script>
+		<script src="themes/wordpress-bootstrap-master/library/js/eass.js"></script>
 	</head>
-	
+
 	<body <?php body_class(); ?>>
-				
+
 		<header role="banner">
-				
+
 			<div class="navbar navbar-default navbar-fixed-top">
 				<div class="container">
-          
+
 					<div class="navbar-header">
 						<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-responsive-collapse">
 							<span class="icon-bar"></span>
@@ -64,7 +68,7 @@
 
 				</div> <!-- end .container -->
 			</div> <!-- end .navbar -->
-		
+
 		</header> <!-- end header -->
-		
+
 		<div class="container">

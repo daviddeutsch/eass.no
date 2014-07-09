145e575c56f4eea5bd66238046b01e85a81afcb4
David Deutsch <skore@valanx.org>
2014-07-09 11:31:30 +0200

    d'oh

--- wordpress/wp-content/themes/wordpress-bootstrap-master/page-left-sidebar.php
+++ wordpress/wp-content/themes/wordpress-bootstrap-master/page-left-sidebar.php
@@ -6,6 +6,26 @@ Template Name: Left Sidebar Page
 
 <?php get_header(); ?>
 
-<?php the_content(); ?>
+			<div class="col-md-12 img-wrap">
+				<img class="placeholder" src="<?php echo get_template_directory_uri(); ?>/library/img/vaktmesterservice.png" alt=""/>
+			</div>
+			<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+						<?php the_content(); ?>
+			<?php endwhile; ?>
+
+			<?php else : ?>
+
+				<article id="post-not-found">
+					<header>
+						<h1><?php _e("Not Found", "wpbootstrap"); ?></h1>
+					</header>
+					<section class="post_content">
+						<p><?php _e("Sorry, but the requested resource was not found on this site.", "wpbootstrap"); ?></p>
+					</section>
+					<footer>
+					</footer>
+				</article>
+
+			<?php endif; ?>
 
 <?php get_footer(); ?>

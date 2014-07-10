e8732ec4c4182c94ceb648a68482fdbfd691db9d
David Deutsch <skore@valanx.org>
2014-07-10 13:34:37 +0200

    value -> jQuery(this)

--- wordpress/wp-content/themes/wordpress-bootstrap-master/library/js/eass.js
+++ wordpress/wp-content/themes/wordpress-bootstrap-master/library/js/eass.js
@@ -74,7 +74,7 @@ eassApp
 				};
 
 				angular.forEach(headers, function(value, key){
-					var heading = value.html();
+					var heading = jQuery(this).html();
 
 					var element = {
 						id: encodeURIComponent(heading),
@@ -85,7 +85,7 @@ eassApp
 						$scope.choices.push(element);
 					}
 
-					var content = value.nextUntil("h1").andSelf();
+					var content = jQuery(this).nextUntil("h1").andSelf();
 
 					content.wrapAll(
 						'<div ng-class="{\''

f2f50db88b018566dd091f2555c753e6046311a8
David Deutsch <skore@valanx.org>
2014-07-10 12:43:01 +0200

    yeah, how about we put this in here.

--- wordpress/wp-content/themes/wordpress-bootstrap-master/library/js/eass.js
+++ wordpress/wp-content/themes/wordpress-bootstrap-master/library/js/eass.js
@@ -50,6 +50,80 @@ function($scope, $timeout) {
 );
 
 eassApp
+	.controller('SidebarPageCtrl',
+		[
+			'$scope', '$location',
+			function($scope, $location) {
+				var headers = $(".panel-body h3" );
+
+				if ( headers.length <= 1 ) {
+					// Nothing to partition
+					return;
+				}
+
+				$scope.choices = [];
+
+				var checkExisting = function( name ) {
+					if ( $scope.choices.length == 0 ) return false;
+
+					for ( var i=0; i<$scope.choices.length; i++ ) {
+						if ( $scope.choices[i].id == name ) return true;
+					}
+
+					return false;
+				};
+
+				angular.forEach(headers, function(value, key){
+					var heading = value.html();
+
+					var element = {
+						id: encodeURIComponent(heading),
+						title: heading
+					};
+
+					if ( !checkExisting(element.id) ) {
+						$scope.choices.push(element);
+					}
+
+					var content = value.nextUntil("h1").andSelf();
+
+					content.wrapAll(
+						'<div ng-class="{\''
+							+ ( $scope.multi ? 'am-slide-top' : 'am-slide-top-fast' )
+							+ '\': isDeselected(\''
+							+ element.id
+							+ '\')}" />'
+					);
+				});
+
+				$scope.id = $location.hash();
+
+				$scope.multi = false;
+
+				$scope.change = function( name ) {
+					if ( $scope.id === name && $scope.multi ) {
+						$scope.id = '';
+					} else if ( $scope.id === name ) {
+						return;
+					} else {
+						$scope.id = name;
+					}
+
+					$location.hash($scope.id);
+				};
+
+				$scope.isDeselected = function ( name ) {
+					return $scope.id !== name && $scope.id != '';
+				};
+
+				$scope.isSelected = function ( name ) {
+					return $scope.id === name;
+				};
+			}
+		]
+	);
+
+eassApp
 .controller('ContactCtrl',
 [
 '$scope',

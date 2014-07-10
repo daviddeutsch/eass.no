7d88bdebda9cd1773292e2d464ff46b97d4ae6ac
David Deutsch <skore@valanx.org>
2014-07-10 12:25:31 +0200

    working on automatic sidebar generation

--- wordpress/wp-content/themes/wordpress-bootstrap-master/page-left-sidebar.php
+++ wordpress/wp-content/themes/wordpress-bootstrap-master/page-left-sidebar.php
@@ -5,12 +5,24 @@ Template Name: Left Sidebar Page
 ?>
 
 <?php get_header(); ?>
-<div id="main">
+		<div id="main" ng-controller="SidebarPageCtrl">
 			<div class="col-md-12 img-wrap">
 				<img class="placeholder" src="<?php echo get_template_directory_uri(); ?>/library/img/vaktmesterservice.png" alt=""/>
 			</div>
 			<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-						<?php the_content(); ?>
+				<div class="col-md-3">
+					<ul class="nav sidenav">
+						<li ng-repeat="choice in choices" ng-click="change(choice.id)" ng-class="{selected: isSelected(choice.id)}">{{choice.title}}</li>
+					</ul>
+				</div>
+
+				<div class="col-md-7">
+					<div class="panel panel-default panel-standalone">
+						<div class="panel-body">
+							<?php the_content(); ?>
+						</div>
+					</div>
+				</div>
 			<?php endwhile; ?>
 
 			<?php else : ?>
@@ -27,5 +39,5 @@ Template Name: Left Sidebar Page
 				</article>
 
 			<?php endif; ?>
-</div>
+		</div>
 <?php get_footer(); ?>

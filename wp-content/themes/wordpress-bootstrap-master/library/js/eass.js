0528176ce59cbec6a66c0e8b39682d3b11ee6dd4
David Deutsch <skore@valanx.org>
2014-08-15 14:06:18 +0200

    trying to go even lower with 2 columns on contact

--- wp-content/themes/wordpress-bootstrap-master/library/js/eass.js
+++ wp-content/themes/wordpress-bootstrap-master/library/js/eass.js
@@ -246,7 +246,7 @@ function($scope, $location, $compile, $timeout)
 					+ ' ng-class="{\'am-slide-top\': isDeselected(\''
 					+ name
 					+ '\')}"'
-					+ ' class="col-sm-6 kontakt-container"'
+					+ ' class="col-xs-6 kontakt-container"'
 					+ '>'
 					+ newhtml
 					+ '</div>'
